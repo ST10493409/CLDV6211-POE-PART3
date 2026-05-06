3ccbbf27a9809c2c90388fc0cc5ce48867e826b5
Steven Keith Milne <st10493409@vcconnect.edu.za>
2026-05-07 00:49:59 +0200

    Added DbContext code and some of the blob code
    Added DbContext and blob code

---
+++ EventEase/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using EventEase.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventEase.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Venue> Venues { get; set; }
+        public DbSet<Event> Events { get; set; }
+        public DbSet<Booking> Booking { get; set; }
+        public DbSet<BookingView> BookingView { get; set; }
+
+
+    }
+}
---
+++ EventEase/Services/BlobService.cs
@@ -0,0 +1,18 @@
+﻿using Azure.Storage.Blobs;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace EventEase.Services
+{
+    public class BlobService
+    {
+        private readonly BlobServiceClient _client;
+
+        public BlobService(string connectionString)
+        {
+            _client = new BlobServiceClient(connectionString);
+        }
+
+        
+    }
+}
