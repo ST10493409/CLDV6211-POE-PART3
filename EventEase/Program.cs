cd7362c8c6630b136672747d6dc60fdc7fba367d
Steven Keith Milne <st10493409@vcconnect.edu.za>
2026-05-07 15:18:18 +0200

    program.cs and json
    added code in both the program.cs and json files

--- EventEase/Program.cs
+++ EventEase/Program.cs
@@ -1,7 +1,17 @@
+using EventEase.Data;
+using EventEase.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer
+(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddSingleton<BlobService>(new BlobService
+    (builder.Configuration.GetConnectionString("AzureBlobStorage")));
 
 var app = builder.Build();
 
