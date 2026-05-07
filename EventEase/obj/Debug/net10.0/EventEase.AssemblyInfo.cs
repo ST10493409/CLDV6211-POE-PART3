99cd4253a6b0c99567bea78c79db0fd5bc05da19
Steven Keith Milne <st10493409@vcconnect.edu.za>
2026-05-07 18:32:23 +0200

    small fix
    just fixed spelling in json file

--- EventEase/obj/Debug/net10.0/EventEase.AssemblyInfo.cs
+++ EventEase/obj/Debug/net10.0/EventEase.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventEase")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd7362c8c6630b136672747d6dc60fdc7fba367d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1bd8ad487ecf5dd26295399b3cbffe715afb780f")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventEase")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventEase")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
