ce1cc6aa56ae374e30ac84ddd063e1cc15204b3e
Steven Keith Milne <st10493409@vcconnect.edu.za>
2026-06-03 16:48:27 +0200

    1
    1

--- EventEase/obj/Debug/net10.0/EventEase.AssemblyInfo.cs
+++ EventEase/obj/Debug/net10.0/EventEase.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventEase")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e068f3038d4b78b537ccd62b65f4c91ff27e752")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e05c8415c7675f4a16d6afb6f26965827b427ded")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventEase")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventEase")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
