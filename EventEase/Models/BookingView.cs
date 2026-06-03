618fc1752602b50749d3f2bf937a1b7e55a210a0
Steven Keith Milne <st10493409@vcconnect.edu.za>
2026-06-03 19:29:18 +0200

    Updated bookingview
    Updated bookingview

--- EventEase/Models/BookingView.cs
+++ EventEase/Models/BookingView.cs
@@ -7,5 +7,6 @@
         public string EventName { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
+        public string EventType { get; set; }
     }
 }
