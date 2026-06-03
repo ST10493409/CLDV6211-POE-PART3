23915e68a196c1886c8f353f19c5a9429b6ce1d6
Steven Keith Milne <st10493409@vcconnect.edu.za>
2026-06-03 22:42:33 +0200

    EventController
    Added a EventController

---
+++ EventEase/Controllers/EventController.cs
@@ -0,0 +1,39 @@
+﻿using EventEase.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventEase.Controllers
+{
+    public class EventsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EventsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index(
+            string search,
+            int? eventTypeId)
+        {
+            var events = _context.Events
+                .Include(e => e.EventType)
+                .AsQueryable();
+
+            if (!string.IsNullOrEmpty(search))
+            {
+                events = events.Where(e =>
+                    e.Name.Contains(search));
+            }
+
+            if (eventTypeId.HasValue)
+            {
+                events = events.Where(e =>
+                    e.EventTypeId == eventTypeId);
+            }
+
+            return View(events.ToList());
+        }
+    }
+}
--- EventEase/obj/Debug/net10.0/EventEase.AssemblyInfo.cs
+++ EventEase/obj/Debug/net10.0/EventEase.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventEase")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+124f86e554fb944c5ef6475942c24dff73bb027c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cadc4d7c985ed7dd8e927528f6835c21a2ab9771")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventEase")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventEase")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
